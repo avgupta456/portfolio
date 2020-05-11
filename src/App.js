ed98bad4cb52a688b18db96b1dc53d00f196e4bc
Abhijit Gupta <avgupta456@gmail.com>
2020-05-11 11:45:43 -0400

    updated particle effect

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import Particles from 'react-particles-js';
 const particle_params = {
   "particles": {
       "number": {"value": 100},
-      "size": {"value": 5},
+      "size": {"value": 4},
   },
   "interactivity": {
-      "events": {
-          "onhover": {
-              "enable": true,
-              "mode": "repulse"
-          }
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": ["grab", "bubble"]
       }
-  }
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {"opacity": 0.5}
+      },
+      "bubble": {
+        "distance": 300,
+        "size": 8,
+        "duration": 1,
+        "opacity": 0.5,
+        "speed": 2
+      },
+    }
+  },
 }
 
 class App extends React.Component {
