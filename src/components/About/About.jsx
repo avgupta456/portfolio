f0b031b4de5d6820c50ca93f5751e973cc87c605
Abhijit Gupta <avgupta456@gmail.com>
2020-05-12 16:51:17 -0400

    working buttons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import { Typography, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Divider, Button } from '@material-ui/core';
 
 import { Profile, TypeLoop } from './..'
 import styles from './About.module.css'
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 export default function About() {
+  const classes = useStyles();
+
   return (
     <div>
       <p className={styles.header}>ABHIJIT GUPTA</p>
@@ -20,12 +29,56 @@ export default function About() {
       <div className={styles.left_column}>
         <div className={styles.text}>
           <Typography>
-          Hello, and welcome to my website. My name is Abhijit Gupta, and I am a junior at Yale University, pursuing a double major in Computer Science and Applied Mathematics. I am a member of the Yale Interactive Machines Group, where I conduct research in the field of human-robot interaction using deep learning techniques. I am a developer for the Yale Computer Society, and help organize YHack, Yale’s largest undergraduate hackathon. This past summer, I interned at RTI international as a data warehouse intern, working on ___. In my free time, I enjoy dancing and rock climbing.
+          Hello, and welcome to my website.
+          My name is Abhijit Gupta, and I am a junior at Yale University, pursuing a double major in Computer Science and Applied Mathematics.
+          I am a member of the Yale Interactive Machines Group, where I conduct research in the field of human-robot interaction using deep learning.
+          I am a developer for the Yale Computer Society, and help organize YHack, Yale’s largest annual hackathon.
+          This past summer, I interned at RTI International as a data warehouse intern, working on ___.
+          In my free time, I enjoy dancing and rock climbing.
           </Typography>
           <br/>
           <Typography>
           I am actively seeking internship opportunities for Summer 2021. Feel free to explore the rest of my website, and please reach out to me if interested!
           </Typography>
+          <br/>
+          <Divider/>
+          <br/>
+          <Button
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+            href="mailto:abhijit.gupta@yale.edu"
+            target="_blank"
+          >
+            <Typography>Email</Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+            href="https://github.com/avgupta456"
+            target="_blank"
+          >
+            <Typography>Github</Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+            href="https://www.linkedin.com/in/avgupta456" 
+            target="_blank"
+          >
+            <Typography>LinkedIn</Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+            href={process.env.PUBLIC_URL + '/resume.pdf'}
+            target='_blank'
+          >
+            <Typography>Resume</Typography>
+          </Button>
         </div>
       </div>
       <div className={styles.right_column}>
