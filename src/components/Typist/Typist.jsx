26770d31df72992a23b87cda6578c2173271a33d
Abhijit Gupta <avgupta456@gmail.com>
2020-05-11 23:57:23 -0400

    cleaned typist

--- src/components/Typist/Typist.jsx
+++ src/components/Typist/Typist.jsx
@@ -8,24 +8,22 @@ import './main.scss';
 
 export default function TypeLoop() {
   return (
-    <TypistLoop interval={3000}>
+    <TypistLoop interval={1500}>
       {[
-        ' Programmer',
-        ' DATA SCIENTIST',
-        ' DANCER',
-        ' PROBLEM SOLVER',
-        ' ROCK CLIMBER',
-        ' RESEARCHER',
+        'Computer Scientist',
+        'Bhangra Dancer',
+        'Data Scientist',
+        'Rock Climber',
+        'Researcher',
       ].map(text =>
         <Typist
           key={text}
           cursor={{blink: true}}
           className={styles.subheader}
-          //className={"TypistExample-header"}
         >
           {text}
-          <Typist.Delay ms={200} />
-          <Typist.Backspace count={text.length-1} delay={200} />
+          <Typist.Delay ms={1000} />
+          <Typist.Backspace count={text.length} delay={200} />
         </Typist>
       )}
     </TypistLoop>
