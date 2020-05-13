5efb1df50dcdf43997636b71ecd48511963ce0ab
Abhijit Gupta <avgupta456@gmail.com>
2020-05-13 11:54:50 -0400

    updated timeline

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,41 +1,32 @@
 import React from 'react';
 
-import {Typography} from '@material-ui/core';
-import WorkIcon from '@material-ui/icons/Work';
+import { makeStyles } from '@material-ui/core/styles';
 import StarIcon from '@material-ui/icons/Star';
+import WorkIcon from '@material-ui/icons/Work';
+import {Typography} from '@material-ui/core';
 
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 import styles from './Timeline.module.css'
 
-function TimelineElement({title, location, date, details, color}) {
-  //Wrapper for VerticalTimelineElement component
-  return (
-    <VerticalTimelineElement
-      className="vertical-timeline-element--work"
-      contentStyle={{ background: `${color}`, color: '#fff' }}
-      contentArrowStyle={{ borderRight: `7px solid  ${color}` }}
-      date={<Typography>{date}</Typography>}
-      dateClassName={styles.date}
-      iconStyle={{ background: `${color}`, color: '#fff' }}
-      icon={<WorkIcon />}
-    >
-      <Typography variant="h6" className="vertical-timeline-element-title">{title}</Typography>
-      <Typography variant="subtitle2" className="vertical-timeline-element-subtitle">{location}</Typography>
-      <Typography>{details}</Typography>
-    </VerticalTimelineElement>
-  )
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography,
+    padding: theme.spacing(1),
+  },
+}));
 
-function TimelineElementCurrent({title, location, date, details, color}) {
-  //Wrapper for VerticalTimelineElement component
+function TimelineElement({title, location, date, details, color}) {
+  const classes = useStyles();
+  
   return (
     <VerticalTimelineElement
       className="vertical-timeline-element--work"
       contentStyle={{ border: `3px solid ${color}`, color: '#000' }}
       contentArrowStyle={{ borderRight: `7px solid  ${color}` }}
-      date={<Typography>{date}</Typography>}
+      date={date}
+      dateClassName={classes.root}
       iconStyle={{ background: `${color}`, color: '#fff' }}
       icon={<WorkIcon />}
     >
@@ -53,10 +44,10 @@ export default function Timeline(props) {
   return (
     <VerticalTimeline
       animate={true}
-      layout={'1-column'}
+      layout={'2-columns'}
       className={styles.line}
     >
-      <TimelineElementCurrent
+      <TimelineElement
         title = "Undergraduate Researcher | Yale University (Interactive Machines Group)"
         location = "New Haven, Connecticut"
         date = "January 2020 - Present"
@@ -88,7 +79,6 @@ export default function Timeline(props) {
         iconStyle={{ background: '#4caf50', color: '#fff' }}
         icon={<StarIcon />}
       />
-
     </VerticalTimeline>
   );
 }
