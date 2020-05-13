02ac1b4646afd85d05b2414ad6a2a386b2c09953
Abhijit Gupta <avgupta456@gmail.com>
2020-05-13 14:52:43 -0400

    Update Education.jsx

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -13,7 +13,10 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
-  tab: {
+  new: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  old: {
     backgroundColor: theme.palette.primary.light,
   },
   icon: {
@@ -44,38 +47,68 @@ export default function Education() {
   return (
     <div className={classes.root}>
       <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <ExpansionPanelSummary className={classes.tab} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
+        <ExpansionPanelSummary className={classes.new} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
           <Typography className={classes.heading}>Yale University</Typography>
           <Typography className={classes.secondaryHeading}>Expected May 2022</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Yale Content Here
-          </Typography>
+          <div>
+            <Typography>
+              Pursuing a double major in <b>Computer Science</b> and <b>Applied Mathematics</b>
+            </Typography>
+            <br/>
+            <Typography>
+              <b>Relavant Coursework:</b> CPSC 223 Data Structures, CPSC 290 Directed Research, MATH 230 Vector Calculus, MATH 244 Discrete Mathematics
+            </Typography>
+            <br/>
+            <Typography>
+              <b>Activities:</b> Yale Computer Society, YHack Leadership, Yale Jashan Bhangra, Yale Climbing Team, Yale Undergraduate Aerospace Assosciation
+            </Typography>
+          </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
       <br/>
       <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <ExpansionPanelSummary className={classes.tab} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
+        <ExpansionPanelSummary className={classes.old} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
           <Typography className={classes.heading}>North Carolina School of Science and Mathematics</Typography>
           <Typography className={classes.secondaryHeading}>August 2017 - May 2019</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
+        <div>
           <Typography>
-            NCSSM Content Here
+            High School Education, focused in STEM fields
           </Typography>
+          <br/>
+          <Typography>
+            <b>Relavant Coursework:</b> Algorithms and Data Structures, Research in Physics, Numerical Analysis, Multivariable Calculus, Mathematical Modeling
+          </Typography>
+          <br/>
+          <Typography>
+            <b>Activities:</b> FIRST Robotics Competition, Computer Science Club President, Math Modeling Competitions
+          </Typography>
+        </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
       <br/>
       <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <ExpansionPanelSummary className={classes.tab} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
+        <ExpansionPanelSummary className={classes.old} expandIcon={<ExpandMoreIcon className={classes.icon}/>}>
           <Typography className={classes.heading}>Panther Creek High School</Typography>
           <Typography className={classes.secondaryHeading}>August 2015 - June 2017</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
+        <div>
           <Typography>
-            Panther Creek Content Here
+            High School Education
           </Typography>
+          <br/>
+          <Typography>
+            <b>Relavant Coursework:</b> AP Computer Science Principles, AP Computer Science, AP Calculus, AP Statistics
+          </Typography>
+          <br/>
+          <Typography>
+            <b>Activities:</b> FIRST Robotics Competition, Math Club President, Science Olympiad
+          </Typography>
+        </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </div>
