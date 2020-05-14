9fde9396061943bd26e6990ca0cb77035fa1a0b9
Abhijit Gupta <avgupta456@gmail.com>
2020-05-13 22:42:13 -0400

    used props

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, CardHeader, Typography, Divider, Paper, Chip, Button } from "@material-ui/core";
+import { Card, CardContent, CardHeader, Typography, Paper, Chip, Button } from "@material-ui/core";
 import styles from './Projects.module.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -22,15 +22,15 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function ChipsArray() {
+function ChipsArray({array}) {
   const classes = useStyles();
-  const [chipData, setChipData] = React.useState([
-    { key: 0, label: 'Angular' },
-    { key: 1, label: 'jQuery' },
-    { key: 2, label: 'Polymer' },
-    { key: 3, label: 'React' },
-    { key: 4, label: 'Vue.js' },
-  ]);
+
+  let new_array = array;
+  if (!new_array) {
+    new_array = [{key:0, label:'Fill In'}];
+  }
+
+  const [chipData] = React.useState(new_array);
 
   return (
     <Paper component="ul" className={classes.root}>
@@ -48,7 +48,7 @@ function ChipsArray() {
   );
 }
 
-function FancyHeaderCard() {
+function FancyHeaderCard({name, date, description, array, github}) {
   const classes = useStyles();
 
   return (
@@ -57,13 +57,18 @@ function FancyHeaderCard() {
         className={styles.header_root}
         title={
           <div>
-            Personal Website
-            <Button variant="contained" className={classes.button}>
+            {name}
+            <Button
+              variant="contained"
+              className={classes.button}
+              href={github}
+              target="_blank"
+            >
               Github
             </Button>
           </div>
         }
-        subheader={"May 2020"}
+        subheader={date}
         classes={{
           title: styles.title,
           subheader: styles.subheader,
@@ -72,11 +77,11 @@ function FancyHeaderCard() {
       <CardContent>
         <div className={styles.inner}>
           <Typography>
-            This is test content blah blah blah This is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blah
+            {description}
           </Typography>
           <br/>
           <div className={styles.chips}>
-            <ChipsArray/>
+            <ChipsArray array={array}/>
           </div>
         </div>
       </CardContent>
@@ -85,20 +90,57 @@ function FancyHeaderCard() {
 }
 
 export default function Projects() {
+  const array = [
+    { key: 0, label: 'Angular' },
+    { key: 1, label: 'jQuery' },
+    { key: 2, label: 'Polymer' },
+    { key: 3, label: 'React' },
+    { key: 4, label: 'Vue.js' },
+  ]
+
+  const name = "Personal Website"
+  const date = "May 2020"
+  const description = "This is test content blah blah blah This is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blah"
+  const github = "temp"
+
   return (
     <div>
       <div className={styles.row}>
         <div className={styles.column}>
-          <FancyHeaderCard/>
+          <FancyHeaderCard
+            name={name}
+            date={date}
+            description={description}
+            array={array}
+            github={github}
+          />
         </div>
         <div className={styles.column}>
-          <FancyHeaderCard className={styles.column}/>
+          <FancyHeaderCard
+            name={name}
+            date={date}
+            description={description}
+            array={array}
+            github={github}
+          />
         </div>
         <div className={styles.column}>
-          <FancyHeaderCard className={styles.column}/>
+          <FancyHeaderCard
+            name={name}
+            date={date}
+            description={description}
+            array={array}
+            github={github}
+          />
         </div>
         <div className={styles.column}>
-          <FancyHeaderCard className={styles.column}/>
+          <FancyHeaderCard
+            name={name}
+            date={date}
+            description={description}
+            array={array}
+            github={github}
+          />
         </div>
       </div>
     </div>
