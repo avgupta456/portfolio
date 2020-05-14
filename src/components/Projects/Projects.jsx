eb2ec8b97ede708ddf40e43267448af263fa01aa
Abhijit Gupta <avgupta456@gmail.com>
2020-05-13 23:11:01 -0400

    added content

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -40,6 +40,7 @@ function ChipsArray({array}) {
             <Chip
               label={data.label}
               className={classes.chip}
+              clickable
             />
           </li>
         );
@@ -60,6 +61,7 @@ function FancyHeaderCard({name, date, description, array, github}) {
             {name}
             <Button
               variant="contained"
+              color="secondary"
               className={classes.button}
               href={github}
               target="_blank"
@@ -90,56 +92,52 @@ function FancyHeaderCard({name, date, description, array, github}) {
 }
 
 export default function Projects() {
-  const array = [
-    { key: 0, label: 'Angular' },
-    { key: 1, label: 'jQuery' },
-    { key: 2, label: 'Polymer' },
-    { key: 3, label: 'React' },
-    { key: 4, label: 'Vue.js' },
-  ]
-
-  const name = "Personal Website"
-  const date = "May 2020"
-  const description = "This is test content blah blah blah This is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blahThis is test content blah blah blah"
-  const github = "temp"
-
   return (
     <div>
       <div className={styles.row}>
         <div className={styles.column}>
           <FancyHeaderCard
-            name={name}
-            date={date}
-            description={description}
-            array={array}
-            github={github}
+            name="DANTE Extension"
+            date="January 2020 - Present"
+            description="Researching the automated detection of conversation structures using computer vision and deep learning techniques. I am currently extending the DANTE algorithm to encorporate input features such as body pose and skeletal markers."
+            array={[{key:0,label:'Python'}, {key:1,label:'TensorFlow'}, {key:2,label:'Deep Learning'}, {key:3,label:'ROS'}]}
+            github="https://github.com/avgupta456/cpsc290"
           />
         </div>
         <div className={styles.column}>
           <FancyHeaderCard
-            name={name}
-            date={date}
-            description={description}
-            array={array}
-            github={github}
+            name="Statbotics"
+            date="August 2019 - Present"
+            description="Developing a website delivering statistics and analysis for the high school FRC robotics competition. Data collection and analysis in Python, displayed with Django/React."
+            array={[{key:0,label:'Python'}, {key:1,label:'Django'}, {key:2,label:'React'}, {key:3,label:'MySQL'}, {key:5,label:'Web APIs'}]}
+            github="https://github.com/avgupta456/Statbotics"
           />
         </div>
         <div className={styles.column}>
           <FancyHeaderCard
-            name={name}
-            date={date}
-            description={description}
-            array={array}
-            github={github}
+            name="YHack Website"
+            date="March 2020 - Present"
+            description="Leading a team of developers revamping the YHack website, used to advertise and accept applications for Yale's largest annual hackathon. Emphasis on updating the tech stack to modern frameworks without sacrificing functionality."
+            array={[{key:0,label:'Django'}, {key:1,label:'React'}, {key:2,label:'Postgres'}, {key:3,label:'Heroku'}]}
+            github="https://github.com/Y-Hack/django-yhack"
           />
         </div>
         <div className={styles.column}>
           <FancyHeaderCard
-            name={name}
-            date={date}
-            description={description}
-            array={array}
-            github={github}
+            name="Personal Website"
+            date="May 2020"
+            description="Built a personal website from scratch using React, HTML/CSS, and the Material-UI component library to display my portfolio and improve my web development skills."
+            array={[{key:0,label:'React'}, {key:1,label:'HTML/CSS'}, {key:2,label:'Material-UI'}, {key:3,label:'CI/CD'}, {key:4,label:'Github Pages'}]}
+            github="https://github.com/avgupta456/personal_website"
+          />
+        </div>
+        <div className={styles.column}>
+          <FancyHeaderCard
+            name="Project Horus"
+            date="September 2019"
+            description="Developed a receipt scanning app to detect healthy spending habits during the 2019 HackMIT event. Won the Instabase API sponsor award and the Financial Technology Track"
+            array={[{key:0,label:'Python'}, {key:1,label:'AWS Textract'}, {key:2,label:'Machine Learning'}, {key:3,label:'NLP/OCR'}, {key:4,label:'HTML/CSS/JS'}]}
+            github="https://github.com/avgupta456/HackMIT-2019"
           />
         </div>
       </div>
