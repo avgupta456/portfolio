a9a0f9b9eb26d0506b55b2bb4be188b33ec2d066
Abhijit Gupta <avgupta456@gmail.com>
2020-05-20 22:15:02 -0400

    update file endings

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-export { default as Background } from './Particles/Particles.jsx';
-export { default as Header } from './Header/Header.jsx';
-export { default as Tabs } from './Tabs/Tabs.jsx';
-export { default as About } from './About/About.jsx';
-export { ButtonBar } from './About/About.jsx';
-export { default as Experience } from './Experience/Experience.jsx';
-export { default as Education } from './Education/Education.jsx';
-export { default as Projects } from './Projects/Projects.jsx';
-export { default as Skills } from './Skills/Skills.jsx';
-export { default as Awards } from './Awards/Awards.jsx'
+export { default as Background } from './Particles/Particles.js';
+export { default as Header } from './Header/Header.js';
+export { default as Tabs } from './Tabs/Tabs.js';
+export { default as About } from './About/About.js';
+export { ButtonBar } from './About/About.js';
+export { default as Experience } from './Experience/Experience.js';
+export { default as Education } from './Education/Education.js';
+export { default as Projects } from './Projects/Projects.js';
+export { default as Skills } from './Skills/Skills.js';
+export { default as Awards } from './Awards/Awards.js'
