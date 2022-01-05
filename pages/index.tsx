c1cd53dc17ff0f2610f070821a1378558a425b76
Abhijit Gupta <avgupta456@gmail.com>
2022-01-05 14:09:41 -0500

    Add Experiences

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ const Home: NextPage = () => (
           ))}
         </div>
       </div>
-      <div className="w-full lg:w-1/3 py-4 lg:py-0 lg:px-4 flex items-center justify-center">
+      <div className="w-full lg:w-1/3 py-4 lg:py-0 lg:pl-8 flex justify-center lg:justify-end">
         <Image
           src="/profile.jpg"
           alt="Abhijit Gupta"
@@ -123,22 +123,63 @@ const Home: NextPage = () => (
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">
       Experiences
     </h1>
-
+    <div className="p-4">
+      During my time at Yale, I have taken two gap semesters to work in industry
+      and further my software skills. In total, I have completed four
+      internships with one upcoming in Summer 2022.
+      <ul className="list-disc pl-8">
+        <li className="p-2">
+          <strong>Quant Trading @ Jane Street (NYC, Summer 2022)</strong>:
+          Upcoming Internship
+        </li>
+        <li className="p-2">
+          <strong>Software Engineering @ Sentry.io (Remote, Fall 2021)</strong>:
+          Worked with the Data Team based in San Francisco building ETL
+          pipelines for 100M+ daily messages from the Sentry.io platform. Used
+          Apache Kafka, PySpark, GCS, AirFlow, BigQuery, and DataDog to
+          prototype a batch processing pipeline with increased throughput and
+          scalability.
+        </li>
+        <li className="p-2">
+          <strong>Quant Trading @ Jane Street (NYC, Summer 2021)</strong>:
+          Attended classes, participated in mock-trading, and worked on two data
+          analysis projects with trading desks. Further details omitted due to
+          IP restrictions.
+        </li>
+        <li className="p-2">
+          <strong>Software Engineering @ Gatherly (Remote, Fall 2020)</strong>:
+          Spearheaded data analytics initiative analyzing 1M+ user actions to
+          extract actionable insights for online event hosts. Implemented an
+          automated payment system using the Stripe API with subscriptions,
+          discounts, and tiered pricing. Used AWS, NodeJS, ReactJS, and Stripe.
+        </li>
+        <li className="p-2">
+          <strong>
+            Data Analyst @ RTI International (Remote, Summer 2020)
+          </strong>
+          : Designed campus monitoring dashboard to visualize temporal building
+          ocupancy. Implemented automated demand prioritization system for BI
+          team, reducing manual labor by 80%. Used Microsoft SQL Server, Power
+          BI, and Excel.
+        </li>
+      </ul>
+    </div>
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">
       Education
     </h1>
-
+    <div className="p-4">These are my education</div>
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">
       Projects
     </h1>
-
+    <div className="p-4">These are my projects</div>
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">Skills</h1>
-
+    <div className="p-4">These are my skills</div>
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">Awards</h1>
-
+    <div className="p-4">These are my awards</div>
     <h1 className="text-3xl font-thin text-gray-800 dark:text-white">
       Contact
     </h1>
+    <div className="p-4">These are my contact</div>
   </div>
 );
 
