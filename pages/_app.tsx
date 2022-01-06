5e945ef1eabf7dd1a32127c7a827ab2430906df9
Abhijit Gupta <avgupta456@gmail.com>
2022-01-05 23:26:50 -0500

    Add Google Analytics (#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,36 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <div>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-145490100-2"
+      />
+
+      <Script
+        id="gtag"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-145490100-2', { page_path: window.location.pathname });
+            `,
+        }}
+      />
+      <ThemeProvider attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </div>
   );
 }
 
