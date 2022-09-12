a6d5a807ce6cb11d3eed1fb1605889182b68017a
Abhijit Gupta <avgupta456@gmail.com>
2022-09-12 10:43:31 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,48 +15,9 @@ const Home: NextPage = () => (
     <div className="p-4 flex flex-wrap">
       <div className="w-full lg:w-2/3">
         Hello! My name is Abhijit Gupta, and I am a{' '}
-        <strong>junior at Yale University</strong>, majoring in Computer Science
-        with a certificate in Statistics and Data Science. At Yale, I have
-        conducted research with the{' '}
-        <a
-          href="https://interactive-machines.gitlab.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
-        >
-          Interactive Machines Group
-        </a>
-        , and previously lead the Tech Team for{' '}
-        <a
-          href="https://yhack.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
-        >
-          YHack
-        </a>{' '}
-        and contributed to the{' '}
-        <a
-          href="https://www.facebook.com/yalecompsociety/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
-        >
-          Yale Computer Society
-        </a>{' '}
-        and{' '}
-        <a
-          href="https://www.yesatyale.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
-        >
-          Yale Entrepreneurial Society
-        </a>
-        . I have previously interned with Jane Street, Sentry.io, Gatherly, and
-        RTI International. I will be interning with Jane Street in Summer 2022
-        and joining full-time in Summer 2023. In my free time, I work on side
-        projects including{' '}
+        <strong>senior at Yale University</strong> majoring in Computer Science.
+        I have previously interned with Jane Street, Sentry.io, Gatherly, and
+        RTI International. In my free time, I work on side projects including{' '}
         <a
           href="https://www.githubtrends.io"
           target="_blank"
@@ -64,15 +25,24 @@ const Home: NextPage = () => (
           className="text-blue-500"
         >
           GitHub Trends
-        </a>{' '}
-        and{' '}
+        </a>
+        ,{' '}
         <a
           href="https://www.statbotics.io"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500"
         >
-          Statbotics.io
+          Statbotics
+        </a>
+        , and{' '}
+        <a
+          href="https://www.hopscotch.nyc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500"
+        >
+          Hopscotch
         </a>
         . Please feel free to reach out!
         <div className="hidden md:flex w-full my-8 items-center justify-center">
@@ -126,14 +96,12 @@ const Home: NextPage = () => (
       Experiences
     </h1>
     <div className="p-4">
-      During my time at Yale, I have taken two gap semesters to work in industry
-      and further my software skills. In total, I have completed four
-      internships with one upcoming in Summer 2022. I have also published two
-      research papers while at Yale.
       <ul className="list-disc pl-8">
         <li className="p-2">
           <strong>Quant Trading @ Jane Street (NYC, Summer 2022)</strong>:
-          Upcoming Internship
+          Attended classes, participated in mock-trading, and worked on data
+          analysis projects with trading desks. Further details omitted due to
+          IP restrictions.
         </li>
         <li className="p-2">
           <strong>Software Engineering @ Sentry.io (Remote, Fall 2021)</strong>:
@@ -198,29 +166,11 @@ const Home: NextPage = () => (
       </ul>
     </div>
     <h1 className="text-3xl font-semibold text-gray-800 dark:text-white">
-      Education
-    </h1>
-    <div className="p-4">
-      I am currently a Junior at Yale University, graduating in May, 2023. I am
-      pursuing a major in Computer Science and a Certificate in Statistics and
-      Data Science.
-      <p className="mt-2 font-semibold">Relevant coursework</p>
-      CPSC 223: Data Structures, CPSC 290 Directed Research, CPSC 323: Systems
-      Programming, CPSC 366: Intensive Algorithms, CPSC 452: Deep Learning, CPSC
-      327: Object Oriented Programming, CPSC 447: Intro to Quantum Computing,
-      CPSC 426: Distributed Systems, MATH 230/231 Vector Calculus and Linear
-      Algebra, MATH 244 Discrete Mathematics, S&DS 230: R Programming.
-      <p className="mt-2 font-semibold">Activities</p>
-      Yale Interactive Machines Group, YHack Organizing Team, Yale Computer
-      Society, Yale Entrepreneurial Society Team
-    </div>
-    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white">
       Projects
     </h1>
     <div className="p-4">
-      Recently, I have worked on GitHub Trends, Statbotics.io, Quickbites,
-      research, club projects, and this portfolio website. More details coming
-      soon!
+      Recently, I have worked on GitHub Trends, Statbotics.io, and Hopscotch.
+      More details coming soon!
     </div>
   </div>
 );
