ef83f7986cbde219c1d57554c2a7167faf3e6aa8
Abhijit Gupta <avgupta456@gmail.com>
2022-11-21 12:17:09 -0500

    Use Document

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="d3d784be-d94e-4a11-b832-8ee6b6f80cfd"
+          src="https://umami-nine-zeta.vercel.app/umami.js"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
